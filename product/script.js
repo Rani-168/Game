b0c92bc1c2e5caf44ccf907b45256d4980cbe8d4
Rani <ranibhosale168@gmail.com>
2026-03-11 21:47:20 +0530

    product web   added

---
+++ product/script.js
@@ -0,0 +1,38 @@
+function loadProducts(){
+
+fetch("https://dummyjson.com/products")
+
+.then(function(response){
+    return response.json();
+})
+
+.then(function(data){
+
+    let products = data.products;
+    let container = document.getElementById("container");
+
+    products.forEach(function(product){
+
+        let card = document.createElement("div");
+        card.className = "card";
+
+        card.innerHTML = `
+        <img src="${product.thumbnail}">
+        <h4>${product.title}</h4>
+        <p>Price: ₹${product.price}</p>
+        <p>Discount: ${product.discountPercentage}%</p>
+        `;
+
+        container.appendChild(card);
+
+    });
+
+})
+
+.catch(function(error){
+    console.log("Error:", error);
+});
+
+}
+
+loadProducts();
